47908cfb84a1487514ee3ecebabee22efe5befc0
zoodogood <zoodogood@gmail.com>
2023-02-15 21:22:24 +0200

    Declare types

--- packages/objectives/CustomCollector.js
+++ packages/objectives/CustomCollector.js
@@ -1,6 +1,20 @@
+/**
+ * @typedef CustomCollectorOptions
+ * @property {object} target
+ * @property {string} event
+ * @property {Function} filter
+ * @property {number} [time=0]
+*/
+
 class CustomCollector {
+
+	/** @type {Function} */
 	#callback;
- 
+	
+	/**
+	 * 
+	 * @param {CustomCollectorOptions} param0 
+	*/
 	constructor({target, event, filter, time = 0}){
 	  if ("on" in target === false){
 		 throw new Error("Target must be similar to EventEmitter.prototype — target haven't method 'on'");
@@ -11,9 +25,14 @@ class CustomCollector {
 	  this.filter = filter;
 	  this.time = time;
 	}
- 
-	setCallback(callback){
 	
+	/**
+	 * 
+	 * @param {Function} callback 
+	 * @returns {void}
+	 */
+	setCallback(callback){
+		
 	  const handler = (...params) => {
 		 const passed = !this.filter || this.filter(params);
  
@@ -25,6 +44,10 @@ class CustomCollector {
 	  this.handle(handler);
 	}
  
+	/**
+	 * @param {Function} handler 
+	 * @returns {void}
+	*/
 	handle(handler){
 	  this.end();
 	  
@@ -49,7 +72,11 @@ class CustomCollector {
 	removeTimeout(){
 	  clearTimeout(this.timeoutId);
 	}
- 
+	
+	/**
+	 * @param {number} ms 
+	 * @returns {void}
+	*/
 	setTimeout(ms){
 	  const callback = this.end.bind(this);
 	  this.timeoutId = setTimeout(callback, ms);

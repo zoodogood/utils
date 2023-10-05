9de074cab9bb14cc721b011dd2b3e069e00f6904
zoodogood <zoodogood@gmail.com>
2023-10-05 16:01:21 +0300

    Rename

---
+++ src/objectives/getRandomNumberInRange.ts
@@ -0,0 +1,17 @@
+interface IGetRandomValueOptions {
+	min?: number;
+	max: number;
+	needRound?: boolean;
+}
+
+
+function getRandomNumberInRange({min = 0, max, needRound = true}: IGetRandomValueOptions){
+	let value = Math.random() * (max - min + Number(needRound)) + min;
+ 
+	if (needRound){
+		value = Math.floor(value);
+	}
+	return value;
+ }
+
+ export { getRandomNumberInRange };

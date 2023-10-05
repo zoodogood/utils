2f9f46ade1c0d45e619acaf1d56851fb5fb43d26
zoodogood <zoodogood@gmail.com>
2023-10-05 16:00:30 +0300

    Improve

--- src/objectives/mod.ts
+++ src/objectives/mod.ts
@@ -1,10 +1,10 @@
 export * from './CustomCollector.js';
 export * from './GlitchText.js';
-export * from './getRandomValue.js';
+export * from './getRandomNumberInRange.js';
 export * from './rangeToArray.js';
 
 
-function omit(object: object, filter: CallableFunction){
+function omit(object: Object, filter: CallableFunction){
 	const entries = Object.entries(object)
 	  .filter(([key, value], i) => filter(key, value, i));
 

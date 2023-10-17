4e4c82d702f3e6e37b3d83aae00bcd6834a29be9
zoodogood <zoodogood@gmail.com>
2023-10-17 17:45:52 +0300

    Fix

--- src/objectives/mod.ts
+++ src/objectives/mod.ts
@@ -2,6 +2,8 @@ export * from './CustomCollector.js';
 export * from './GlitchText.js';
 export * from './getRandomNumberInRange.js';
 export * from './rangeToArray.js';
+export * from './getRandomElementFromArray.js';
+export * from './DotNotatedInterface.js';
 
 
 function omit(object: Object, filter: CallableFunction){

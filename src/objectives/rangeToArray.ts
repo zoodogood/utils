623cbbd3ffcf506efc50bc1ba51ffabd5a33914e
zoodogood <zoodogood@gmail.com>
2024-01-09 10:43:42 +0200

    Improve

--- src/objectives/rangeToArray.ts
+++ src/objectives/rangeToArray.ts
@@ -1,6 +1,11 @@
-export function rangeToArray([min, max]: [number, number]){
-	if (isNaN(min) || isNaN(max)){
-		return NaN;
-	}
-	return [...new Array(max - min + 1)].map((_, index) => index + min);
+export function* rangeToArray([min, max]: [number, number]) {
+  if (isNaN(min) || isNaN(max)) {
+    yield NaN;
+    return;
+  }
+  const count = max - min + 1;
+  for (let index = 0; index < count; index++) {
+    yield index + min;
+  }
+  return;
 }

bf342b45bd06a21ade61847294e6c118383f8e0c
zoodogood <zoodogood@gmail.com>
2024-01-26 09:06:13 +0200

    Improve

--- tests/primitives/BracketsParser.test.ts
+++ tests/primitives/BracketsParser.test.ts
@@ -1,11 +1,11 @@
 import { BracketsParser } from "../../src/primitives/BracketsParser.js";
 import { expect, test } from "vitest";
-test(() => {
+test("Nested groups", () => {
   const text = '({}[{"nested"}])';
   const parser = new BracketsParser();
   const result = parser
     .addBracketVariant(...BracketsParser.defaultBracketVariants)
     .parse(text);
 
-  expect(result.groups).toHaveLength(4);
+  expect(result.groups).toHaveLength(5);
 });

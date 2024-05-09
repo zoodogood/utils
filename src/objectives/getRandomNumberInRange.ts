7869aa78a0b8e6fa69ca2c3c313f5b837899b5e0
zoodogood <zoodogood@gmail.com>
2024-05-09 14:41:08 +0300

    Clean a code

--- src/objectives/getRandomNumberInRange.ts
+++ src/objectives/getRandomNumberInRange.ts
@@ -1,17 +1,20 @@
 interface IGetRandomValueOptions {
-	min?: number;
-	max: number;
-	needRound?: boolean;
+  min?: number;
+  max: number;
+  needRound?: boolean;
 }
 
+function getRandomNumberInRange({
+  min = 0,
+  max,
+  needRound = true,
+}: IGetRandomValueOptions) {
+  let value = Math.random() * (max - min + Number(needRound)) + min;
 
-function getRandomNumberInRange({min = 0, max, needRound = true}: IGetRandomValueOptions){
-	let value = Math.random() * (max - min + Number(needRound)) + min;
- 
-	if (needRound){
-		value = Math.floor(value);
-	}
-	return value;
- }
+  if (needRound) {
+    value = Math.floor(value);
+  }
+  return value;
+}
 
- export { getRandomNumberInRange };
+export { getRandomNumberInRange };

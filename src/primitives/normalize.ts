714429e855ba21fd24794268dd94b3e3dc095669
zoodogood <zoodogood@gmail.com>
2024-12-03 12:08:11 +0200

    Rename; Change algorithm

--- src/primitives/normalize.ts
+++ src/primitives/normalize.ts
@@ -1,5 +1,7 @@
-export function normalize_to_integer(list: number[]) {
-  // (1 / 3) * 1e15 === 333 333 333 333 333.3
-  const MIN_OPTIMAL_VALUE = 1e16;
-  return list.map((x) => Math.floor(x * MIN_OPTIMAL_VALUE));
+export function normalize_to_max_coefficient(list: number[]) {
+  // normalize_to_optimal(2, MAX_SAFE_INTEGER) will be coefficient 1:9007199254740990
+  // normalize_to_optimal(0.25, 3000) will be coefficient 750537393112:9006448717347879
+  const sum = list.reduce((acc, value) => acc + value, 0);
+  const multiplayer = Number.MAX_SAFE_INTEGER / sum;
+  return list.map((x) => Math.floor(x * multiplayer));
 }

859b7fd66c6bcb64f2f5fccb77b7f09fe4fed4f6
zoodogood <zoodogood@gmail.com>
2025-01-06 10:01:56 +0200

    Fix

--- src/discordjs/chain/get_chain_array.ts
+++ src/discordjs/chain/get_chain_array.ts
@@ -1,10 +1,21 @@
-import type { Client, TextChannel } from "discord.js";
+import type { Client, Message, SendableChannels } from "discord.js";
 import { chain_map } from "./map.js";
 
-export async function get_chain_array(partId: string, client: Client) {
-	const [channelId, parts] = chain_map.get(partId);
+export async function get_chain_array(
+	partId: string,
+	client: Client,
+): Promise<Array<Message | { id: string }>> {
+	const [channelId, ...parts] = chain_map.get(partId)!;
 	const channel = client.channels.cache.get(channelId);
-	return Promise.all(
-		parts.map((id: string) => (channel as TextChannel).messages.fetch(id)),
+
+	return await Promise.all(
+		parts.map((id: string) =>
+			(channel as SendableChannels).messages.fetch(id).catch((error) => {
+				if (error.message !== "Unknown Message") {
+					throw error;
+				}
+				return { id };
+			}),
+		),
 	);
 }

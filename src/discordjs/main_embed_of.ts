6558578fb3a3c756a61812c9049ec42b258691b5
zoodogood <zoodogood@gmail.com>
2025-01-06 09:59:45 +0200

    Allow APIEmbed

--- src/discordjs/main_embed_of.ts
+++ src/discordjs/main_embed_of.ts
@@ -1,5 +1,16 @@
-import type { Embed, Message } from "discord.js";
+import type { APIEmbed } from "discord-api-types/v10";
+import type {
+	Embed,
+	JSONEncodable,
+	Message,
+	MessageCreateOptions,
+} from "discord.js";
 
-export function main_embed_of(message: Message): Embed {
-	return message.embeds[0];
+export function main_embed_of(
+	message: Message | MessageCreateOptions,
+): Embed | APIEmbed | undefined {
+	return message.embeds?.[0] as Exclude<
+		Embed | APIEmbed | undefined,
+		JSONEncodable<APIEmbed>
+	>;
 }

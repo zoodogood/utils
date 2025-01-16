d16e4ef02c8ec961bbd4a688dd4009165d91cbbf
zoodogood <zoodogood@gmail.com>
2025-01-16 07:30:52 +0200

    Improve types

--- src/discordjs/modal.ts
+++ src/discordjs/modal.ts
@@ -2,11 +2,14 @@ import type { APIModalInteractionResponseCallbackData } from "discord-api-types/
 
 import {
 	ComponentType,
-	type Interaction,
 	ModalBuilder,
+	type ModalSubmitInteraction,
+	type RepliableInteraction,
+	type TextInputComponent,
 	type TextInputComponentData,
 	TextInputStyle,
 } from "discord.js";
+import { assert } from "node:assert";
 import { justComponents } from "./message_components.js";
 
 export function createModal({
@@ -25,17 +28,22 @@ export function createModal({
 export async function justModalQuestion({
 	title,
 	customId = "modal",
-	components,
+	components: _components,
 	interaction,
 	thanks = false,
 }: {
 	title: string;
 	customId?: string;
 	components: Parameters<typeof justComponents>[0];
-	interaction: Interaction;
+	interaction: Exclude<RepliableInteraction, ModalSubmitInteraction>;
 	thanks?: string | boolean;
 }) {
-	components = components.map(
+	assert(
+		!("type" in _components && _components.type === ComponentType.ActionRow),
+	);
+	!Array.isArray(_components) && (_components = [_components]);
+	const components: TextInputComponent[] = _components.map(
+		// @ts-expect-error
 		(addable: Partial<TextInputComponentData>, i = 0) => ({
 			type: ComponentType.TextInput,
 			customId: `${customId}_content_${i}`,
@@ -54,8 +62,9 @@ export async function justModalQuestion({
 
 	await interaction.showModal(modal);
 	const response = await interaction.awaitModalSubmit({
-		filter: (interaction) => customId === interaction.customId,
-		time: MINUTE * 5,
+		filter: (interaction: ModalSubmitInteraction) =>
+			customId === interaction.customId,
+		time: 60_000 /*MINUTE*/ * 5,
 	});
 
 	thanks &&

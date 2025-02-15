79c06e18f30713f5dadb32c285c7f68ff82125fd
zoodogood <zoodogood@gmail.com>
2025-02-15 07:05:02 +0200

    Improve test

--- src/discordjs/chain/move_partial_data_to_chain.test.ts
+++ src/discordjs/chain/move_partial_data_to_chain.test.ts
@@ -1,5 +1,5 @@
-import { test } from "vitest";
-import { type AdvancedPayload, diagnosticLimits } from "../mod";
+import { expect, test } from "vitest";
+import { type AdvancedPayload, createMessage, diagnosticLimits } from "../mod";
 import { move_partial_data_to_chain } from "./move_partial_data_to_chain";
 
 test("move_partial_data_to_chain", () => {
@@ -12,6 +12,35 @@ test("move_partial_data_to_chain", () => {
 		payload,
 		{ separateBy: "\n", splay: "balanced" },
 		diagnostic,
-		false,
 	);
+});
+
+test("Atomic chunk too big", () => {
+	const payload: AdvancedPayload = {
+		content: `${"x".repeat(3000)}\n${"y".repeat(3000)}`,
+	};
+	const diagnostic = diagnosticLimits(payload);
+	expect(() =>
+		move_partial_data_to_chain(
+			payload,
+			{ separateBy: "\n", splay: "balanced" },
+			diagnostic,
+		),
+	).toThrow();
+});
+
+test("Content and description different behavior?", () => {
+	const ALPHABET_A_CODE = 97;
+	const description = `${Array.from(
+		new Array(8),
+		(_, i) => `${String.fromCharCode(i + ALPHABET_A_CODE).repeat(1000)}\n`,
+	).join("")}\n\`\`\` а эта часть должна оказаться вне код блока`;
+	createMessage({
+		maySplitMessage: {
+			separateBy: "\n",
+			splay: "balanced",
+		},
+		footer: { text: "i18n is not defined" },
+		content: description,
+	});
 });

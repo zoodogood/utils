d5d6e1b7960b754519fea100ff35b0b7f8a533e0
zoodogood <zoodogood@gmail.com>
2025-02-15 08:00:18 +0200

    Fix typo

--- src/discordjs/chain/configuration.ts
+++ src/discordjs/chain/configuration.ts
@@ -3,5 +3,5 @@ import type { DiscordContentLimites } from "../message_content_limits.js";
 export interface MaySplitConfiguration {
 	fields?: (keyof typeof DiscordContentLimites)[];
 	separateBy?: string;
-	splay: "balanced" | "rest";
+	splay?: "balanced" | "rest";
 }

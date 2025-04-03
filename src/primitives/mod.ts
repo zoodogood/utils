549bab758bd98c8e46411bb039e448f12cb9e8ce
zoodogood <zoodogood@gmail.com>
2025-04-03 11:55:16 +0300

    Move ending to pluralization; Export pluralization

--- src/primitives/mod.ts
+++ src/primitives/mod.ts
@@ -3,35 +3,10 @@ export * from "./BracketsParser.js";
 export { CliParser } from "./CliParser.js";
 export * from "./createDefaultPreventable.js";
 export * from "./normalize.js";
+export * from './pluralization/index.js';
 export * from "./symbols.js";
 export * from "./TextTableBuilder.js";
 
-interface IEndingOptions {
-	unite?: (quantity: number, word: string) => string;
-}
-export function ending(
-	quantity: number,
-	base: string,
-	multiple: string,
-	alone: string,
-	double: string,
-	options: IEndingOptions = {},
-) {
-	if (Number.isNaN(+quantity)) {
-		return Number.NaN;
-	}
-
-	const target = quantity % 100 < 20 ? quantity % 20 : quantity % 10;
-
-	const end =
-		target >= 5 || target === 0 ? multiple : target > 1 ? double : alone;
-	const word = base + end;
-
-	options.unite ||= (quantity, word) => `${quantity} ${word}`;
-
-	const input = options.unite(quantity, word);
-	return input;
-}
 
 export function sortMutByResolve<T>(
 	array: T[],

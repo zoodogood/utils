13287abb64ee8b95ae641545fa197d0363aaf061
zoodogood <zoodogood@gmail.com>
2026-06-06 15:58:42 +0300

    fix: types

--- src/objectives/makeArray.ts
+++ src/objectives/makeArray.ts
@@ -1,3 +1,3 @@
-export function makeArray<T>(mapFn: (a: number) => T[], length: number) {
+export function makeArray<T>(mapFn: (a: number) => T, length: number): T[] {
 	return Array.from({ length }, (_, i) => mapFn(i)) as T[]
 }
